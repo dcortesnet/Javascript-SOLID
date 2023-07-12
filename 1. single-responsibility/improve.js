278b8c4986baef5c6137f3976a37de31c7adae18
dcortesnet <dcortes.net@gmail.com>
2023-07-12 11:56:33 -0400

    Single responsibility

---
+++ 1. single-responsibility/improve.js
@@ -0,0 +1,15 @@
+// Case to improve
+
+class UserService {
+  constructor() {
+    // ...
+  }
+
+  authenticateUser(username, password) {
+    // logic authenticated
+  }
+
+  sendEmail(user, subject, message) {
+    // logic send email
+  }
+}
---
+++ 1. single-responsibility/improved.js
@@ -0,0 +1,21 @@
+// Case improved and apply the principle
+
+class AuthenticationService {
+  constructor() {
+    // ...
+  }
+
+  authenticateUser(username, password) {
+    // logic authenticated
+  }
+}
+
+class EmailService {
+  constructor() {
+    // ...
+  }
+
+  sendEmail(user, subject, message) {
+    // logic send email
+  }
+}
